edf0b97ce48617c543dff3de5e02152ffe86379d
lavanyaalanka <168562297+lavanyaalanka@users.noreply.github.com>
2025-08-04 14:47:23 +0530

    Update jobRoutes.js

--- server/routes/jobRoutes.js
+++ server/routes/jobRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const authMiddleware = require('../middlewares/authMiddleware');
 const Job = require('../models/Job');
-const User = require('../models/User');
+const User = require('../models/user');
 
 // ✅ POST: Only Admin can create a job
 router.post('/', authMiddleware, async (req, res) => {
